155383160d337da64886b5609933ece800dd6c93
Christian Kümmel <c.kuemmel@xdev-software.de>
2022-06-13 23:50:30 +0200

    fill controller with comments

--- src/main/java/one/microstream/controller/BookController.java
+++ src/main/java/one/microstream/controller/BookController.java
@@ -9,56 +9,60 @@ import one.microstream.domain.Book;
 import one.microstream.storage.DB;
 import one.microstream.utils.MockupUtils;
 
-
 @Controller("/books")
-public class BookController
-{
+public class BookController {
 	@Get("/create")
-	public HttpResponse<?> createBooks()
-	{
+	public HttpResponse<?> createBooks() {
 		List<Book> loadMockupData = MockupUtils.loadMockupData();
-		
+
 		DB.root.getBooks().addAll(loadMockupData);
 		DB.storageManager.store(DB.root.getBooks());
-		
+
 		return HttpResponse.ok("Books successfully created!");
 	}
-	
+
 	@Get
-	public List<Book> getBook()
-	{
+	public List<Book> getBook() {
 		return DB.root.getBooks();
 	}
-	
+
 	@Get("/startsWith_A")
-	public List<Book> getBooksWithA()
-	{
-		// Enter your code here
-		
+	public List<Book> getBooksWithA() {
+//		return DB.root.getBooks().stream().filter(b -> b.getName().startsWith("A")).collect(Collectors.toList());
 		return null;
 	}
-	
+
 	@Get("/clear")
-	public HttpResponse<?> clearBooks()
-	{
-		// Enter your code here
-		
+	public HttpResponse<?> clearBooks() {
+//		DB.root.getBooks().clear();
+//		DB.storageManager.store(DB.root.getBooks());
+
 		return HttpResponse.ok("Books successfully cleared!");
 	}
-	
+
 	@Get("/updateSingle")
-	public HttpResponse<?> updateSingleBook()
-	{
-		// Enter your code here
-		
+	public HttpResponse<?> updateSingleBook() {
+//		Book book = DB.root.getBooks().stream().findFirst().get();
+//		book.setRelease(LocalDate.now());
+//		
+//		DB.storageManager.store(book);
+
 		return HttpResponse.ok("Book successfully updated!");
 	}
-	
+
 	@Get("/updateMulti")
-	public HttpResponse<?> updateMultiBooks()
-	{
-		// Enter your code here
-		
+	public HttpResponse<?> updateMultiBooks() {
+//		Storer es = DB.storageManager.createEagerStorer();
+//
+//		DB.root.getBooks().stream().filter(b -> b.getName().startsWith("A")).forEach(b -> {
+//			BigDecimal value = b.getPrice().multiply(new BigDecimal(0.9));
+//			b.setPrice(value);
+//
+//			es.store(es);
+//		});
+//
+//		es.commit();
+
 		return HttpResponse.ok("Bookss successfully updated!");
 	}
 }
